50b81b5c641cc77d7a1194c6b2bf0bc7a7824e64
D10NGYANG <D10NGYANG@163.com>
2023-05-01 13:35:53 +0800

    ci: 增加私仓发布；

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -89,6 +89,9 @@ dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 }
 
+val bds100MavenUsername: String by project
+val bds100MavenPassword: String by project
+
 afterEvaluate {
     publishing {
         publications {
@@ -100,6 +103,13 @@ afterEvaluate {
         repositories {
             maven {
                 url = uri("/Users/d10ng/project/kotlin/maven-repo/repository")
+            }
+            maven {
+                credentials {
+                    username = bds100MavenUsername
+                    password = bds100MavenPassword
+                }
+                setUrl("https://nexus.bds100.com/repository/maven-releases/")
             }
         }
     }
