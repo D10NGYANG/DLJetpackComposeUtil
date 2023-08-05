9ca9c08abf2852446738e92c2a28d069016e5b71
D10NGYANG <D10NGYANG@163.com>
2023-08-05 16:53:17 +0800

    feat: update kotlin 1.9.0, gradle 8.2.1, android 8.1.0, compose 1.5.1;

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,23 @@ plugins {
     id("com.android.application") version android_build_ver apply false
     id("com.android.library") version android_build_ver apply false
     id("org.jetbrains.kotlin.android") version kotlin_ver apply false
+    id("com.github.ben-manes.versions") version "0.47.0"
 }
 
 tasks.register<Delete>(name = "clean") {
     group = "build"
     delete(rootProject.buildDir)
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
 }
