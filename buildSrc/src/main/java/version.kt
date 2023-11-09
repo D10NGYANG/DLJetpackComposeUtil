c0da58a92ee5590c6a1b7cef1cbe8c57c8e1fccf
D10NGYANG <D10NGYANG@163.com>
2023-11-09 18:45:29 +0800

    feat: 更新kotlin 1.9.20;

--- buildSrc/src/main/java/version.kt
+++ buildSrc/src/main/java/version.kt
@@ -4,11 +4,11 @@ object Project {
     const val target_sdk = 30
 }
 
-const val android_build_ver = "8.1.2"
+const val android_build_ver = "8.1.3"
 const val compose_ver = "androidx.compose:compose-bom:2023.10.01"
-const val compose_compiler_ver = "1.5.3"
-const val kotlin_ver = "1.9.10"
-const val ksp_ver = "1.9.10-1.0.13"
+const val compose_compiler_ver = "1.5.4"
+const val kotlin_ver = "1.9.20"
+const val ksp_ver = "1.9.20-1.0.14"
 const val kotlin_coroutines_ver = "1.7.3"
 const val kotlin_serialization_json = "1.6.0"
 const val jetpack_lifecycle_ver = "2.6.2"
