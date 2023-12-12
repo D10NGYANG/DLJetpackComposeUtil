87b2bfd2045522b7ed9b012ca8cf931271caf917
D10NGYANG <D10NGYANG@163.com>
2023-12-12 17:35:52 +0800

    feat: update kotlin 1.9.21;

--- buildSrc/src/main/java/version.kt
+++ buildSrc/src/main/java/version.kt
@@ -8,19 +8,19 @@ const val androidx_core_ver = "1.12.0"
 const val jetpack_lifecycle_ver = "2.6.2"
 
 // kotlin
-const val kotlin_ver = "1.9.20"
-const val ksp_ver = "1.9.20-1.0.14"
+const val kotlin_ver = "1.9.21"
+const val ksp_ver = "1.9.21-1.0.15"
 // kotlin ex
 const val kotlin_coroutines_ver = "1.7.3"
 const val kotlin_serialization_json = "1.6.2"
 
 // compose
 const val compose_ver = "androidx.compose:compose-bom:2023.10.01"
-const val compose_compiler_ver = "1.5.5"
+const val compose_compiler_ver = "1.5.6"
 // compose ex
 const val compose_destinations_ver = "1.9.55"
 
 // d10ng
-const val dl_app_ver = "2.4.13"
-const val dl_date_ver = "1.8.7"
-const val dl_common_ver = "0.0.6"
+const val dl_app_ver = "2.4.14"
+const val dl_date_ver = "1.9.0"
+const val dl_common_ver = "0.0.7"
--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.D10NGYANG"
-version = "2.0.12"
+version = "2.0.13"
 
 android {
     namespace = "com.d10ng.compose"
