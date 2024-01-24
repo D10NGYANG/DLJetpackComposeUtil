c17a2e478606727e7a3f1e38ca93a0060db2509c
D10NGYANG <D10NGYANG@163.com>
2024-01-24 15:06:14 +0800

    feat: 更新依赖，kotlin 1.9.22，ksp 1.9.22-1.0.17, compose compiler 1.5.8, android build 8.2.2;

--- buildSrc/src/main/java/version.kt
+++ buildSrc/src/main/java/version.kt
@@ -2,23 +2,23 @@
 const val android_compile_sdk = 34
 const val android_min_sdk = 24
 const val android_target_sdk = 30
-const val android_build_ver = "8.2.1"
+const val android_build_ver = "8.2.2"
 const val androidx_core_ver = "1.12.0"
 // jetpack
-const val jetpack_lifecycle_ver = "2.6.2"
+const val jetpack_lifecycle_ver = "2.7.0"
 
 // kotlin
-const val kotlin_ver = "1.9.21"
-const val ksp_ver = "1.9.21-1.0.15"
+const val kotlin_ver = "1.9.22"
+const val ksp_ver = "1.9.22-1.0.17"
 // kotlin ex
 const val kotlin_coroutines_ver = "1.7.3"
 const val kotlin_serialization_json = "1.6.2"
 
 // compose
 const val compose_ver = "androidx.compose:compose-bom:2023.10.01"
-const val compose_compiler_ver = "1.5.7"
+const val compose_compiler_ver = "1.5.8"
 // compose ex
-const val compose_destinations_ver = "1.9.57"
+const val compose_destinations_ver = "1.9.62"
 
 // d10ng
 const val dl_app_ver = "2.4.14"
