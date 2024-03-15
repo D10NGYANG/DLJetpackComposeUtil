4b9ec6ff611e3ed2f6b2393d92431a48443d30dd
D10NGYANG <D10NGYANG@163.com>
2024-03-15 17:00:51 +0800

    feat: 更新依赖;

--- buildSrc/src/main/java/version.kt
+++ buildSrc/src/main/java/version.kt
@@ -15,12 +15,12 @@ const val kotlin_coroutines_ver = "1.8.0"
 const val kotlin_serialization_json = "1.6.3"
 
 // compose
-const val compose_ver = "androidx.compose:compose-bom:2024.02.01"
+const val compose_ver = "androidx.compose:compose-bom:2024.02.02"
 const val compose_compiler_ver = "1.5.10"
 // compose ex
-const val compose_destinations_ver = "1.10.1"
+const val compose_destinations_ver = "1.10.2"
 
 // d10ng
-const val dl_app_ver = "2.5.1"
+const val dl_app_ver = "2.5.2"
 const val dl_date_ver = "1.9.1"
 const val dl_common_ver = "0.0.9"
