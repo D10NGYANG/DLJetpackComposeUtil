5081265fbdcafc8133a827553679a33b78faf053
D10NGYANG <D10NGYANG@163.com>
2024-05-07 13:58:27 +0800

    feat: update compose-bom:2024.05.00;compose_compiler:1.5.13;androidx_core:1.13.1;

--- buildSrc/src/main/java/version.kt
+++ buildSrc/src/main/java/version.kt
@@ -2,8 +2,8 @@
 const val android_compile_sdk = 34
 const val android_min_sdk = 26
 const val android_target_sdk = 30
-const val android_build_ver = "8.3.2"
-const val androidx_core_ver = "1.13.0"
+const val android_build_ver = "8.4.0"
+const val androidx_core_ver = "1.13.1"
 // jetpack
 const val jetpack_lifecycle_ver = "2.7.0"
 
@@ -15,8 +15,8 @@ const val kotlin_coroutines_ver = "1.8.0"
 const val kotlin_serialization_json = "1.6.3"
 
 // compose
-const val compose_ver = "androidx.compose:compose-bom:2024.04.01"
-const val compose_compiler_ver = "1.5.12"
+const val compose_ver = "androidx.compose:compose-bom:2024.05.00"
+const val compose_compiler_ver = "1.5.13"
 // compose ex
 const val compose_destinations_ver = "1.10.2"
 
